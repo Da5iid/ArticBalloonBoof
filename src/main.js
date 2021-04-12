c45c506d206f388590cdbec0e5038f02e47e8f35
Da5iid <75045999+Da5iid@users.noreply.github.com>
2021-04-12 04:20:59 -0700

    completed version (no sound)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    scene: [Menu, Play],
 }
 
 let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 16;
+let borderPadding = borderUISize / 3;
+
+let keyLEFT, keyRIGHT, keyF, keyR;
