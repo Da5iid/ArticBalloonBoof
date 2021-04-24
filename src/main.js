0a7c07edc1691d8cfd8c9f9970b9481ce3af577b
Da5iid <75045999+Da5iid@users.noreply.github.com>
2021-04-23 21:16:13 -0700

    added balloons and background music, WIP

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 console.log("I am linked");
 
 
-//this is the unmodified version of the game, in the process of adding new stuff!
+//this is the slightly modded version of ghetto space invaders (now with sound!), in the process of adding new stuff!
+
+/* 
+Currently added:
+Balloon assets
+colorful blue sky background
+hell music, written and recorded by yours truly
+
+to do:
+add cute puffy clouds
+change rocket to throwing dart
+change sound effects
+change balloon popping animation
+make dart steerable (tilt left and right instead of sliding)
+make dart faster
+add trees on either side of the screen
+vodka button
+maybe 2 players :)
+
+*/
 let config = {
     type: Phaser.CANVAS,
     width: 640,
